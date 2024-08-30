17f7c1fcd7cdf77032a4a0d0fc92f1ce9f4302bf
Md-Sanzid-Bin-Hossain <145281630+Md-Sanzid-Bin-Hossain@users.noreply.github.com>
2024-08-30 06:08:57 -0400

    Add files via upload

---
+++ Codes/dataset_A_subject_selection.py
@@ -0,0 +1,258 @@
+main_dir = "/home/sanzidpr/Journal_8/Dataset A/Subject01"
+os.mkdir(main_dir) 
+subject='Subject_01'
+path='/home/sanzidpr/Journal_8/Dataset A/Subject01/'
+
+train_data_hof=np.concatenate((subject_2_data_hof,subject_3_data_hof,subject_4_data_hof,subject_5_data_hof,subject_6_data_hof,
+                               subject_7_data_hof,subject_8_data_hof,subject_9_data_hof,subject_10_data_hof),axis=0)
+
+train_data_IMU_Kin=np.concatenate((subject_2_data_IMU_Kin,subject_3_data_IMU_Kin,subject_4_data_IMU_Kin,subject_5_data_IMU_Kin,subject_6_data_IMU_Kin,
+                               subject_7_data_IMU_Kin,subject_8_data_IMU_Kin,subject_9_data_IMU_Kin,subject_10_data_IMU_Kin),axis=0)
+                               
+
+test_data_hof=subject_1_data_hof
+test_data_IMU_Kin=subject_1_data_IMU_Kin
+
+
+
+#########################################################################################################################################################################################
+
+
+
+main_dir = "/home/sanzidpr/Journal_8/Dataset A/Subject02"
+os.mkdir(main_dir) 
+subject='Subject_02'
+path='/home/sanzidpr/Journal_8/Dataset A/Subject02/'
+
+train_data_hof=np.concatenate((subject_1_data_hof,subject_3_data_hof,subject_4_data_hof,subject_5_data_hof,subject_6_data_hof,
+                               subject_7_data_hof,subject_8_data_hof,subject_9_data_hof,subject_10_data_hof),axis=0)
+
+train_data_IMU_Kin=np.concatenate((subject_1_data_IMU_Kin,subject_3_data_IMU_Kin,subject_4_data_IMU_Kin,subject_5_data_IMU_Kin,subject_6_data_IMU_Kin,
+                               subject_7_data_IMU_Kin,subject_8_data_IMU_Kin,subject_9_data_IMU_Kin,subject_10_data_IMU_Kin),axis=0)
+                               
+
+test_data_hof=subject_2_data_hof
+test_data_IMU_Kin=subject_2_data_IMU_Kin
+
+
+
+#########################################################################################################################################################################################
+
+
+
+
+main_dir = "/home/sanzidpr/Journal_8/Dataset A/Subject03"
+os.mkdir(main_dir) 
+subject='Subject_03'
+path='/home/sanzidpr/Journal_8/Dataset A/Subject03/'
+
+train_data_hof=np.concatenate((subject_1_data_hof,subject_2_data_hof,subject_4_data_hof,subject_5_data_hof,subject_6_data_hof,
+                               subject_7_data_hof,subject_8_data_hof,subject_9_data_hof,subject_10_data_hof),axis=0)
+
+train_data_IMU_Kin=np.concatenate((subject_1_data_IMU_Kin,subject_2_data_IMU_Kin,subject_4_data_IMU_Kin,subject_5_data_IMU_Kin,subject_6_data_IMU_Kin,
+                               subject_7_data_IMU_Kin,subject_8_data_IMU_Kin,subject_9_data_IMU_Kin,subject_10_data_IMU_Kin),axis=0)
+                               
+
+test_data_hof=subject_3_data_hof
+test_data_IMU_Kin=subject_3_data_IMU_Kin
+
+
+
+#########################################################################################################################################################################################
+
+
+
+
+main_dir = "/home/sanzidpr/Journal_8/Dataset A/Subject04"
+os.mkdir(main_dir) 
+subject='Subject_04'
+path='/home/sanzidpr/Journal_8/Dataset A/Subject04/'
+
+train_data_hof=np.concatenate((subject_1_data_hof,subject_2_data_hof,subject_3_data_hof,subject_5_data_hof,subject_6_data_hof,
+                               subject_7_data_hof,subject_8_data_hof,subject_9_data_hof,subject_10_data_hof),axis=0)
+
+train_data_IMU_Kin=np.concatenate((subject_1_data_IMU_Kin,subject_2_data_IMU_Kin,subject_3_data_IMU_Kin,subject_5_data_IMU_Kin,subject_6_data_IMU_Kin,
+                               subject_7_data_IMU_Kin,subject_8_data_IMU_Kin,subject_9_data_IMU_Kin,subject_10_data_IMU_Kin),axis=0)
+                               
+
+test_data_hof=subject_4_data_hof
+test_data_IMU_Kin=subject_4_data_IMU_Kin
+
+
+
+#########################################################################################################################################################################################
+
+
+
+main_dir = "/home/sanzidpr/Journal_8/Dataset A/Subject05"
+os.mkdir(main_dir) 
+subject='Subject_05'
+path='/home/sanzidpr/Journal_8/Dataset A/Subject05/'
+
+train_data_hof=np.concatenate((subject_1_data_hof,subject_2_data_hof,subject_3_data_hof,subject_4_data_hof,subject_6_data_hof,
+                               subject_7_data_hof,subject_8_data_hof,subject_9_data_hof,subject_10_data_hof),axis=0)
+
+train_data_IMU_Kin=np.concatenate((subject_1_data_IMU_Kin,subject_2_data_IMU_Kin,subject_3_data_IMU_Kin,subject_4_data_IMU_Kin,subject_6_data_IMU_Kin,
+                               subject_7_data_IMU_Kin,subject_8_data_IMU_Kin,subject_9_data_IMU_Kin,subject_10_data_IMU_Kin),axis=0)
+                               
+
+test_data_hof=subject_5_data_hof
+test_data_IMU_Kin=subject_5_data_IMU_Kin
+
+
+
+#########################################################################################################################################################################################
+
+
+main_dir = "/home/sanzidpr/Journal_8/Dataset A/Subject06"
+os.mkdir(main_dir) 
+subject='Subject_06'
+path='/home/sanzidpr/Journal_8/Dataset A/Subject06/'
+
+train_data_hof=np.concatenate((subject_1_data_hof,subject_2_data_hof,subject_3_data_hof,subject_4_data_hof,subject_5_data_hof,
+                               subject_7_data_hof,subject_8_data_hof,subject_9_data_hof,subject_10_data_hof),axis=0)
+
+train_data_IMU_Kin=np.concatenate((subject_1_data_IMU_Kin,subject_2_data_IMU_Kin,subject_3_data_IMU_Kin,subject_4_data_IMU_Kin,subject_5_data_IMU_Kin,
+                               subject_7_data_IMU_Kin,subject_8_data_IMU_Kin,subject_9_data_IMU_Kin,subject_10_data_IMU_Kin),axis=0)
+                               
+
+test_data_hof=subject_6_data_hof
+test_data_IMU_Kin=subject_6_data_IMU_Kin
+
+
+
+#########################################################################################################################################################################################
+
+
+
+main_dir = "/home/sanzidpr/Journal_8/Dataset A/Subject07"
+os.mkdir(main_dir) 
+subject='Subject_07'
+path='/home/sanzidpr/Journal_8/Dataset A/Subject07/'
+
+train_data_hof=np.concatenate((subject_1_data_hof,subject_2_data_hof,subject_3_data_hof,subject_4_data_hof,subject_5_data_hof,
+                               subject_6_data_hof,subject_8_data_hof,subject_9_data_hof,subject_10_data_hof),axis=0)
+
+train_data_IMU_Kin=np.concatenate((subject_1_data_IMU_Kin,subject_2_data_IMU_Kin,subject_3_data_IMU_Kin,subject_4_data_IMU_Kin,subject_5_data_IMU_Kin,
+                               subject_6_data_IMU_Kin,subject_8_data_IMU_Kin,subject_9_data_IMU_Kin,subject_10_data_IMU_Kin),axis=0)
+                               
+
+test_data_hof=subject_7_data_hof
+test_data_IMU_Kin=subject_7_data_IMU_Kin
+
+
+
+#########################################################################################################################################################################################
+
+
+
+
+main_dir = "/home/sanzidpr/Journal_8/Dataset A/Subject08"
+os.mkdir(main_dir) 
+subject='Subject_08'
+path='/home/sanzidpr/Journal_8/Dataset A/Subject08/'
+
+train_data_hof=np.concatenate((subject_1_data_hof,subject_2_data_hof,subject_3_data_hof,subject_4_data_hof,subject_5_data_hof,
+                               subject_6_data_hof,subject_7_data_hof,subject_9_data_hof,subject_10_data_hof),axis=0)
+
+train_data_IMU_Kin=np.concatenate((subject_1_data_IMU_Kin,subject_2_data_IMU_Kin,subject_3_data_IMU_Kin,subject_4_data_IMU_Kin,subject_5_data_IMU_Kin,
+                               subject_6_data_IMU_Kin,subject_7_data_IMU_Kin,subject_9_data_IMU_Kin,subject_10_data_IMU_Kin),axis=0)
+                               
+
+test_data_hof=subject_8_data_hof
+test_data_IMU_Kin=subject_8_data_IMU_Kin
+
+
+
+#########################################################################################################################################################################################
+
+
+main_dir = "/home/sanzidpr/Journal_8/Dataset A/Subject09"
+os.mkdir(main_dir) 
+subject='Subject_09'
+path='/home/sanzidpr/Journal_8/Dataset A/Subject09/'
+
+train_data_hof=np.concatenate((subject_1_data_hof,subject_2_data_hof,subject_3_data_hof,subject_4_data_hof,subject_5_data_hof,
+                               subject_6_data_hof,subject_7_data_hof,subject_8_data_hof,subject_10_data_hof),axis=0)
+
+train_data_IMU_Kin=np.concatenate((subject_1_data_IMU_Kin,subject_2_data_IMU_Kin,subject_3_data_IMU_Kin,subject_4_data_IMU_Kin,subject_5_data_IMU_Kin,
+                               subject_6_data_IMU_Kin,subject_7_data_IMU_Kin,subject_8_data_IMU_Kin,subject_10_data_IMU_Kin),axis=0)
+                               
+
+test_data_hof=subject_9_data_hof
+test_data_IMU_Kin=subject_9_data_IMU_Kin
+
+
+
+#########################################################################################################################################################################################
+
+
+
+main_dir = "/home/sanzidpr/Journal_8/Dataset A/Subject10"
+os.mkdir(main_dir) 
+subject='Subject_10'
+path='/home/sanzidpr/Journal_8/Dataset A/Subject10/'
+
+train_data_hof=np.concatenate((subject_1_data_hof,subject_2_data_hof,subject_3_data_hof,subject_4_data_hof,subject_5_data_hof,
+                               subject_6_data_hof,subject_7_data_hof,subject_8_data_hof,subject_9_data_hof),axis=0)
+
+train_data_IMU_Kin=np.concatenate((subject_1_data_IMU_Kin,subject_2_data_IMU_Kin,subject_3_data_IMU_Kin,subject_4_data_IMU_Kin,subject_5_data_IMU_Kin,
+                               subject_6_data_IMU_Kin,subject_7_data_IMU_Kin,subject_8_data_IMU_Kin,subject_9_data_IMU_Kin),axis=0)
+                               
+
+test_data_hof=subject_10_data_hof
+test_data_IMU_Kin=subject_10_data_IMU_Kin
+
+
+
+#########################################################################################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Codes/dataset_B_subject_selection.py
@@ -0,0 +1,371 @@
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id01"
+os.mkdir(main_dir) 
+subject='id01'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id01/'
+
+
+train_data=np.concatenate((id02_data,id03_data,id04_data,id05_data,id07_data,id08_data,id10_data,id12_data,id13_data,
+                            id14_data,id15_data,id16_data,id17_data,id18_data,id19_data,id22_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id01_data
+
+
+
+##########################################################################################################################################################################################
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id02"
+os.mkdir(main_dir) 
+subject='id02'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id02/'
+
+
+train_data=np.concatenate((id01_data,id03_data,id04_data,id05_data,id07_data,id08_data,id10_data,id12_data,id13_data,
+                            id14_data,id15_data,id16_data,id17_data,id18_data,id19_data,id22_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id02_data
+
+
+
+##########################################################################################################################################################################################
+
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id03"
+os.mkdir(main_dir) 
+subject='id03'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id03/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id04_data,id05_data,id07_data,id08_data,id10_data,id12_data,id13_data,
+                            id14_data,id15_data,id16_data,id17_data,id18_data,id19_data,id22_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id03_data
+
+
+
+##########################################################################################################################################################################################
+
+
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id04"
+os.mkdir(main_dir) 
+subject='id04'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id04/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id05_data,id07_data,id08_data,id10_data,id12_data,id13_data,
+                            id14_data,id15_data,id16_data,id17_data,id18_data,id19_data,id22_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id04_data
+
+
+
+##########################################################################################################################################################################################
+
+
+
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id05"
+os.mkdir(main_dir) 
+subject='id05'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id05/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id04_data,id07_data,id08_data,id10_data,id12_data,id13_data,
+                            id14_data,id15_data,id16_data,id17_data,id18_data,id19_data,id22_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id05_data
+
+
+
+##########################################################################################################################################################################################
+
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id07"
+os.mkdir(main_dir) 
+subject='id07'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id07/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id04_data,id05_data,id08_data,id10_data,id12_data,id13_data,
+                            id14_data,id15_data,id16_data,id17_data,id18_data,id19_data,id22_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id07_data
+
+
+
+##########################################################################################################################################################################################
+
+
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id08"
+os.mkdir(main_dir) 
+subject='id08'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id08/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id04_data,id05_data,id07_data,id10_data,id12_data,id13_data,
+                            id14_data,id15_data,id16_data,id17_data,id18_data,id19_data,id22_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id08_data
+
+
+
+##########################################################################################################################################################################################
+
+
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id10"
+os.mkdir(main_dir) 
+subject='id10'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id10/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id04_data,id05_data,id07_data,id08_data,id12_data,id13_data,
+                            id14_data,id15_data,id16_data,id17_data,id18_data,id19_data,id22_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id10_data
+
+
+
+##########################################################################################################################################################################################
+
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id12"
+os.mkdir(main_dir) 
+subject='id12'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id12/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id04_data,id05_data,id07_data,id08_data,id10_data,id13_data,
+                            id14_data,id15_data,id16_data,id17_data,id18_data,id19_data,id22_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id12_data
+
+
+
+##########################################################################################################################################################################################
+
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id13"
+os.mkdir(main_dir) 
+subject='id13'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id13/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id04_data,id05_data,id07_data,id08_data,id10_data,id12_data,
+                            id14_data,id15_data,id16_data,id17_data,id18_data,id19_data,id22_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id13_data
+
+
+
+##########################################################################################################################################################################################
+
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id14"
+os.mkdir(main_dir) 
+subject='id14'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id14/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id04_data,id05_data,id07_data,id08_data,id10_data,id12_data,
+                            id13_data,id15_data,id16_data,id17_data,id18_data,id19_data,id22_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id14_data
+
+
+
+##########################################################################################################################################################################################
+
+
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id15"
+os.mkdir(main_dir) 
+subject='id15'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id15/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id04_data,id05_data,id07_data,id08_data,id10_data,id12_data,
+                            id13_data,id14_data,id16_data,id17_data,id18_data,id19_data,id22_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id15_data
+
+
+
+##########################################################################################################################################################################################
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id16"
+os.mkdir(main_dir) 
+subject='id16'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id16/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id04_data,id05_data,id07_data,id08_data,id10_data,id12_data,
+                            id13_data,id14_data,id15_data,id17_data,id18_data,id19_data,id22_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id16_data
+
+
+
+##########################################################################################################################################################################################
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id17"
+os.mkdir(main_dir) 
+subject='id17'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id17/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id04_data,id05_data,id07_data,id08_data,id10_data,id12_data,
+                            id13_data,id14_data,id15_data,id16_data,id18_data,id19_data,id22_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id17_data
+
+
+
+##########################################################################################################################################################################################
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id18"
+os.mkdir(main_dir) 
+subject='id18'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id18/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id04_data,id05_data,id07_data,id08_data,id10_data,id12_data,
+                            id13_data,id14_data,id15_data,id16_data,id17_data,id19_data,id22_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id18_data
+
+
+
+##########################################################################################################################################################################################
+
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id19"
+os.mkdir(main_dir) 
+subject='id19'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id19/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id04_data,id05_data,id07_data,id08_data,id10_data,id12_data,
+                            id13_data,id14_data,id15_data,id16_data,id17_data,id18_data,id22_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id19_data
+
+
+##########################################################################################################################################################################################
+
+
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id22"
+os.mkdir(main_dir) 
+subject='id22'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id22/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id04_data,id05_data,id07_data,id08_data,id10_data,id12_data,
+                            id13_data,id14_data,id15_data,id16_data,id17_data,id18_data,id19_data,id23_data,id24_data,id25_data),axis=0)
+
+
+test_data=id22_data
+
+
+##########################################################################################################################################################################################
+
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id23"
+os.mkdir(main_dir) 
+subject='id23'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id23/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id04_data,id05_data,id07_data,id08_data,id10_data,id12_data,
+                            id13_data,id14_data,id15_data,id16_data,id17_data,id18_data,id19_data,id22_data,id24_data,id25_data),axis=0)
+
+
+test_data=id23_data
+
+
+##########################################################################################################################################################################################
+
+
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id24"
+os.mkdir(main_dir) 
+subject='id24'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id24/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id04_data,id05_data,id07_data,id08_data,id10_data,id12_data,
+                            id13_data,id14_data,id15_data,id16_data,id17_data,id18_data,id19_data,id22_data,id23_data,id25_data),axis=0)
+
+
+test_data=id24_data
+
+
+##########################################################################################################################################################################################
+
+
+
+
+main_dir = "/home/sanzidpr/HICCS_submission/Dataset B/id25"
+os.mkdir(main_dir) 
+subject='id25'
+path='/home/sanzidpr/HICCS_submission/Dataset B/id25/'
+
+
+train_data=np.concatenate((id01_data,id02_data,id03_data,id04_data,id05_data,id07_data,id08_data,id10_data,id12_data,
+                            id13_data,id14_data,id15_data,id16_data,id17_data,id18_data,id19_data,id22_data,id23_data,id24_data),axis=0)
+
+
+test_data=id25_data
+
+
+##########################################################################################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
